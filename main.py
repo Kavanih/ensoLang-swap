a8914372470289b85701ccb251b104499506123b
Kavanih <116088413+Kavanih@users.noreply.github.com>
2023-07-30 16:48:22 +0100

    Update main.py
    Implementing Aave

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import requests
 from web3 import Web3
-import os
+import os, json
 import dotenv
+from w3 import (
+    provider,
+    approve,
+    fork_chain,
+    teardown,
+    get_token_balance,
+    address as from_address,
+)
 
 dotenv.load_dotenv()
 
@@ -10,12 +18,14 @@ privateKey = os.getenv("PRIVATE_KEY")
 
 w3 = Web3(Web3.HTTPProvider(node_url))
 
+
 account = w3.eth.account.from_key(privateKey)
 usdt = "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
 usdc = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
 arb = "0x912CE59144191C1204E64559FE8253a0e49E6548"
 chain_id = w3.eth.chain_id
 amount_in = 1 * 10**18
+vitalik = w3.to_checksum_address(from_address)
 
 
 class EnsoFinance:
@@ -64,7 +74,7 @@ class EnsoFinance:
         w3.eth.wait_for_transaction_receipt(res)
         return True
 
-    def swap_one(self, chain_id, account, token_in, amount, token_out):
+    def swap(self, chain_id, account, token_in, amount, token_out):
         self.approve(chain_id, account, token_in, amount)
         _url = f"{self.base_url}/shortcuts/route"
         print(account.address)
@@ -108,6 +118,90 @@ class EnsoFinance:
         res = w3.eth.send_raw_transaction(tnx.rawTransaction)
         print(f"Swap done: trx hash: {res.hex()}")
 
+    def borrow(self, chain_id, from_address):
+        walletRes = self.account_wallet(from_address, chain_id)
+        wallet = w3.to_checksum_address(walletRes["address"])
+        ausdc = w3.to_checksum_address("0x9ba00d6856a4edf4665bca2c2309936572473b7e")
+        aaveV2LendingPool = w3.to_checksum_address(
+            "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
+        )
+        usdc = w3.to_checksum_address("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
+        dai = w3.to_checksum_address("0x6b175474e89094c44da98b954eedeac495271d0f")
+        usdcAmount = w3.to_wei("1000", "mwei")
+        daiAmount = w3.to_wei("500", "ether")
+        print(approve(usdc, wallet))
+        data = [
+            {
+                "protocol": "erc20",
+                "action": "transferfrom",
+                "args": {
+                    "sender": from_address,
+                    "recipient": wallet,
+                    "token": usdc,
+                    "amount": usdcAmount,
+                },
+            },
+            {
+                "protocol": "aave-v2",
+                "action": "deposit",
+                "args": {
+                    "tokenIn": usdc,
+                    "tokenOut": ausdc,
+                    "amountIn": usdcAmount,
+                    "primaryAddress": aaveV2LendingPool,
+                },
+            },
+            {
+                "protocol": "aave-v2",
+                "action": "borrow",
+                "args": {
+                    "collateral": usdc,
+                    "tokenOut": dai,
+                    "amountOut": daiAmount,
+                    "primaryAddress": aaveV2LendingPool,
+                    "toEao": True,
+                },
+            },
+        ]
+        params = {
+            "chainId": chain_id,
+            "fromAddress": from_address,
+            "toEoa": True,
+        }
+        res = requests.post(
+            f"https://api.enso.finance/api/v1/shortcuts/bundle",
+            params=params,
+            data=json.dumps(data),
+            headers={
+                "Authorization": f"Bearer {self.key}",
+                "Content-Type": "application/json",
+            },
+        )
+        data = res.json()
+        tx = data.get("tx")
+        tx["value"] = int(tx["value"])
+        tx["gasPrice"] = provider.eth.gas_price
+        tx["gas"] = 10000000000
+        tx["nonce"] = provider.eth.get_transaction_count(from_address)
+        tx["gas"] = provider.eth.estimate_gas(tx)
+        balb4 = get_token_balance(dai, wallet)
+        trx = provider.eth.send_transaction(tx)
+        res = provider.eth.wait_for_transaction_receipt(trx)
+        print(res["status"])
+        balaf = get_token_balance(dai, wallet)
+        print(balaf - balb4)
+
 
 enso = EnsoFinance()
-enso.swap_one(chain_id, account, arb, amount_in, usdt)
+# enso.swap(chain_id, account, arb, amount_in, usdt)
+snapshot_id = None
+try:
+    snapshot_id = fork_chain()
+    enso.borrow(1, vitalik)
+except Exception as e:
+    print(e)
+
+finally:
+    if snapshot_id:
+        print(snapshot_id)
+        teardown(snapshot_id)
